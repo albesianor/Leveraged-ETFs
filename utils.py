0cfa3edc0fbc6bb9b8cc413da8299e49f5c59eef
Roberto Albesiano <roberto.albesiano@gmail.com>
2026-01-30 12:16:49 -0500

    refactor: move gbm generator to `utils.py`

---
+++ utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def simulate_gbm(S0, mu, sigma, T, N, n_paths, leverage):
+    # simulate n_paths many paths from 0 to T of GBM with parameters mu and sigma
+    dt = T / N
+    times = np.linspace(0, T, N + 1)
+
+    Z = np.random.randn(n_paths, N)
+
+    drift = mu - (1 / 2) * (sigma**2) * dt
+    diffusion = sigma * np.sqrt(dt) * Z
+
+    logS = np.zeros((n_paths, N + 1))
+    logS[:, 0] = np.log(S0)
+    logS[:, 1:] = np.cumsum(drift + diffusion, axis=1) + logS[:, :1]
+
+    # leveraged log-returns
+    logL = np.zeros((n_paths, N + 1))
+    logL[:, 0] = logS[:, 0]
+    logL[:, 1:] = (
+        np.cumsum(np.log((np.exp(drift + diffusion) - 1) * leverage + 1), axis=1)
+        + logL[:, :1]
+    )
+
+    S_end = np.exp(logS[:, N])
+    L_end = np.exp(logL[:, N])
+    L_end[np.isnan(L_end)] = 0
+
+    return L_end - S_end
